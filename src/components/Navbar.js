47d2506cdabc29d49756ad8143e010125b2387a1
tharsis-soares <50808840+tharsis-soares@users.noreply.github.com>
2021-04-09 00:50:01 -0300

    Navbar Setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React from "react"
+import logo from "../caramelo-logo-3.png"
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg back-color">
+            <div className="container">
+                <a className="navbar-brand" href="/home"><img className="logo" src={logo} alt="logo..."/></a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Sobre</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Projetos</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Contato</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
